377dfada1e0f5cf8fa5f3ceebe1387d7fb450f29
Moeki Kawakami <moeki.kawakami@icloud.com>
2024-10-17 18:38:08 +0900

    Update main visual

--- app/components/header_component.rb
+++ app/components/header_component.rb
@@ -1,6 +1,6 @@
 class HeaderComponent < Clapton::Component
   def render
-    c(:box, { class: "h-16 w-full fixed z-[50] bg-white flex items-center justify-between px-5 border-b" })
+    c(:box, { class: "h-16 w-full fixed bg-white flex items-center justify-between px-5 border-b z-[1000]" })
       .add(c(:h, 1, { class: "font-bold" }).add(c(:text, "Clapton")))
       .add(c(:a, "https://github.com/kawakamimoeki/clapton", { class: "flex items-center" })
         .add(c(:embed, "<img src=\"/img/github-mark.svg\" alt=\"GitHub\" class=\"w-6 h-6\">")))
